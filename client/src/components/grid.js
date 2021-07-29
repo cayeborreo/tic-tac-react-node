5d0a94ee20c84ed6c551baa42f973285cfb3765e
cayeborreo <mcgborreo@gmail.com>
2021-07-29 16:22:11 +0800

    deps(fix): add sass, bulma

--- client/src/components/grid.js
+++ client/src/components/grid.js
@@ -1,22 +1,7 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import GridSlot from "./grid-slot";
 
 const Grid = ({ grid, handleClick }) => {
-  // <div className="column is-4">
-  //   <div className="box m-1">
-  //     <figure className="image is-square">
-  //       <img src={x} alt="X" />
-  //     </figure>
-  //   </div>
-  // </div>
-  // <div className="column is-4">
-  //   <div className="box m-1">
-  //     <figure className="image is-square">
-  //       <img src={o} alt="X" />
-  //     </figure>
-  //   </div>
-  // </div>
-
   return (
     <div className="columns grid is-multiline is-gapless is-mobile">
       {!!grid && grid?.length > 0
